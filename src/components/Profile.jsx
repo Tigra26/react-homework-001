77c506f7224c126638387efbfe2099bf3258f662
Alena Shykova <shining8smaragd@gmail.com>
2023-12-19 19:38:49 +0200

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,30 +1,30 @@
 export const Profile = () => {
-  
+
   return (
     <div class="profile">
     <div class="description">
       <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        src="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
         alt="User avatar"
         class="avatar"
       />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
+      <p class="name">Jacques Gluke</p>
+      <p class="tag">jgluke</p>
+      <p class="location">Ocho Rios, Jamaica</p>
     </div>
   
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
-        <span class="quantity">1000</span>
+        <span class="quantity">5603</span>
       </li>
       <li>
         <span class="label">Views</span>
-        <span class="quantity">2000</span>
+        <span class="quantity">4827</span>
       </li>
       <li>
         <span class="label">Likes</span>
-        <span class="quantity">3000</span>
+        <span class="quantity">1308</span>
       </li>
     </ul>
   </div>
